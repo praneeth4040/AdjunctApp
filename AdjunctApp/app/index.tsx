1afbae361781f9dc8a4d123ab93e05909c6f03d3
Praneeth <praneethchakka23@gmail.com>
2025-08-06 18:32:41 +0530

    Initial project setup with Expo, including configuration files, dependencies, and basic app structure. Added .gitignore, package.json, and README.md for project documentation. Included essential assets and TypeScript configuration.

---
+++ AdjunctApp/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View,StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#ffffff',
+    alignItems:'center',
+    justifyContent:'center',
+
+  }
+})
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
